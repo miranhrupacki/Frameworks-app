923a6fe451a4ead1608433ba2a9156a776cf756f
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-13 09:02:19 +0200

    Initial Commit

---
+++ SwiftUI-SA-Frameworks/SwiftUI_SA_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SA_FrameworksApp.swift
+//  SwiftUI-SA-Frameworks
+//
+//  Created by Miran Hrupački on 13.06.2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SA_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
