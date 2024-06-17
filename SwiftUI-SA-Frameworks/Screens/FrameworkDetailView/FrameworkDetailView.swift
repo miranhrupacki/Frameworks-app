867c5b15d74784b27265968f3e96dad21369c8ba
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-17 14:15:27 +0200

    practicing layout

--- SwiftUI-SA-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
+++ SwiftUI-SA-Frameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
@@ -9,26 +9,44 @@ import SwiftUI
 
 struct FrameworkDetailView: View {
     let framework: Framework
-    @Binding var isShowingDetailView: Bool
     @State private var isShowingSafariView: Bool = false
     
     var body: some View {
-        VStack {            
-            Spacer()
-
-            FrameworkTitleView(framework: framework)
-
+        ScrollView {
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+            
+            ZStack {
+                Image(framework.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .background(LinearGradient(gradient: Gradient(colors: [.clear, .gray]), startPoint: .top, endPoint: .bottom))
+                    .clipShape(.buttonBorder)
+            }
+            
+            .frame(maxWidth: .infinity)
+            
             Text(framework.description)
                 .font(.body)
                 .padding()
-
+            
             Spacer()
-
+            
             Button {
                 isShowingSafariView = true
             } label: {
-                AFButton(title: "Learn More")
+//                AFButton(title: "Learn More")
+                Label("Learn More", systemImage: "book.fill")
             }
+            .buttonStyle(.bordered)
+            .controlSize(.large)
+            .tint(.red)
         }
         .fullScreenCover(isPresented: $isShowingSafariView, content: {
             SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
@@ -37,5 +55,5 @@ struct FrameworkDetailView: View {
 }
 
 #Preview {
-    FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
+    FrameworkDetailView(framework: MockData.sampleFramework)
 }
