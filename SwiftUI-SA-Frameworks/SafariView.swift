bf0efd7197c26a8c1ac6335fc22a657da2f03c07
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-17 09:51:34 +0200

    safari view for link handling in app

--- SwiftUI-SA-Frameworks/SafariView.swift
+++ SwiftUI-SA-Frameworks/SafariView.swift
@@ -5,4 +5,19 @@
 //  Created by Miran Hrupački on 14.06.2024..
 //
 
-import Foundation
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> some SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+//    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) { }
+}
