fecc849fcbf156b9f638e01773ea93da04959b27
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-17 09:51:56 +0200

    changed initial view

--- SwiftUI-SA-Frameworks/SwiftUI_SA_FrameworksApp.swift
+++ SwiftUI-SA-Frameworks/SwiftUI_SA_FrameworksApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_SA_FrameworksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworkGridView()
         }
     }
 }
