ba1fa124fc493966f54469872b8caf8aa895b2ba
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-17 09:48:59 +0200

    reusable button with logic and ui

--- SwiftUI-SA-Frameworks/AFButton.swift
+++ SwiftUI-SA-Frameworks/AFButton.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct AFButton: View {
+    let title: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .foregroundStyle(.white)
+            .frame(width: 280, height: 50)
+            .background(.red)
+            .clipShape(.buttonBorder)
     }
 }
 
 #Preview {
-    AFButton()
+    AFButton(title: "Test Title")
 }
